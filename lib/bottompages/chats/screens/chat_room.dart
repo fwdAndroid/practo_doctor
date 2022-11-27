f577acdf1d53426b1fe84940d4b44620fa729a64
fwdAndroid <fwdkaleem@gmail.com>
2022-11-27 12:49:15 +0500

    Doctor Chat part 2

--- lib/bottompages/chats/screens/chat_room.dart
+++ lib/bottompages/chats/screens/chat_room.dart
@@ -14,6 +14,7 @@ class ChatRoom extends StatefulWidget {
 class _ChatRoomState extends State<ChatRoom> {
   @override
   Widget build(BuildContext context) {
+    TextEditingController controller = TextEditingController();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -60,10 +61,77 @@ class _ChatRoomState extends State<ChatRoom> {
                 ),
               ),
             ),
-            buildChatComposer()
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              color: Colors.white,
+              height: 100,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 5),
+                      height: 60,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: TextField(
+                              controller: controller,
+                              decoration: InputDecoration(
+                                suffixIcon: Row(
+                                  mainAxisAlignment: MainAxisAlignment
+                                      .spaceBetween, // added line
+                                  mainAxisSize: MainAxisSize.min, // added line
+                                  children: <Widget>[
+                                    Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Icon(Icons.attach_file)),
+                                  ],
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(20)),
+                                  // width: 0.0 produces a thin "hairline" border
+                                  borderSide: const BorderSide(
+                                      color: Colors.blue, width: 0.0),
+                                ),
+                                border: const OutlineInputBorder(),
+                                fillColor: Color(0xffF1F1F1),
+                                filled: true,
+                                hintText: 'Type Something.....',
+                                hintStyle: TextStyle(color: Colors.grey[500]),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          InkWell(
+                            onTap: () {
+                              sendMessage(controller.text.trim(), 0);
+                            },
+                            child: Container(
+                              height: 40,
+                              width: 40,
+                              decoration: BoxDecoration(
+                                  color: Colors.blue, shape: BoxShape.circle),
+                              child: Icon(
+                                Icons.send,
+                                color: Colors.white,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
     );
   }
+  
+  void sendMessage(String trim, int i) {}
 }
--- lib/bottompages/chats/widgets/widgets.dart
+++ lib/bottompages/chats/widgets/widgets.dart
@@ -1,3 +1,2 @@
 export './all_chat.dart';
 export './conversation.dart';
-export './chat_composer.dart';
