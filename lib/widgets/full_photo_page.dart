dec101617e2d3d6d6ddd4096362b180fa32cc748
fwdAndroid <fwdkaleem@gmail.com>
2023-01-11 20:15:57 +0500

    Image is Downloaded

--- lib/widgets/full_photo_page.dart
+++ lib/widgets/full_photo_page.dart
@@ -1,25 +1,33 @@
-import 'dart:io';
-
-import 'package:firebase_storage/firebase_storage.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:photo_view/photo_view.dart';
 import 'package:gallery_saver/gallery_saver.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:path_provider/path_provider.dart';
 
-class FullPhotoPage extends StatelessWidget {
+class FullPhotoPage extends StatefulWidget {
   final String url;
 
   FullPhotoPage({Key? key, required this.url}) : super(key: key);
 
   @override
+  State<FullPhotoPage> createState() => _FullPhotoPageState();
+}
+
+class _FullPhotoPageState extends State<FullPhotoPage> {
+  @override
   Widget build(BuildContext context) {
-    Reference file;
     return Scaffold(
       appBar: AppBar(
         actions: [
           IconButton(
               onPressed: () async {
-                //         await downloadFile(file, context);
+                final tempDir = await getTemporaryDirectory();
+                final path = '${tempDir.path}/myfile.png';
+                await Dio().download(widget.url, path);
+                GallerySaver.saveImage(path);
+                ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text("Image is Downloaded")));
               },
               icon: Icon(
                 Icons.download,
@@ -34,17 +42,9 @@ class FullPhotoPage extends StatelessWidget {
       ),
       body: Container(
         child: PhotoView(
-          imageProvider: NetworkImage(url),
+          imageProvider: NetworkImage(widget.url),
         ),
       ),
     );
-  }
-
-  downloadFile(Reference reference, BuildContext context) async {
-    final dir = await getApplicationDocumentsDirectory();
-    final file = File('${dir.path}/${reference.name}');
-    await reference.writeToFile(file);
-    ScaffoldMessenger.of(context)
-        .showSnackBar(SnackBar(content: Text("download Complete")));
   }
 }
